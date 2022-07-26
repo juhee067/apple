ef9bd87ea16389a91c2291aa8bfe8cfb8df30795
Your Namewngmlchoi@naver.comwngmlchoi@naver.co <wngmlchoi@naver.com>
2022-07-26 17:44:50 +0900

    20220726

--- apple/js/apple.js
+++ apple/js/apple.js
@@ -1,3 +1,16 @@
 $(function () {
-  $(".main_slider .container").slick({ arrows: false });
+  $(".main_slider").slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  $('.main_slider').on('afterChange', function (e, s, c) {
+    $('.main_visual .bar').addClass('on')
+  });
+  $('.main_slider').on('beforeChange', function () {
+    $('.main_visual .bar').removeClass('on')
+  });
+
 });
