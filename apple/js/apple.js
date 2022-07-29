400808fc07b2b836073570cadfab6fea8246130e
Your Namewngmlchoi@naver.comwngmlchoi@naver.co <wngmlchoi@naver.com>
2022-07-29 12:23:21 +0900

    20220729

--- apple/js/apple.js
+++ apple/js/apple.js
@@ -12,6 +12,21 @@ $(function () {
   $(".main_slider").on("beforeChange", function () {
     $(".main_visual .bar").removeClass("on");
   });
-
-
+  $(".compare_base").slick({
+    arrows: false,
+    //autoplay: true,
+    dots: true,
+    customPaging: function (slider, i, src) {
+      var thumb = $(slider.$slides[i]).data();
+      src = i + 1;
+      if (i == "0") {
+        i = "Series 7";
+      } else if (i == "1") {
+        i = "Series 3";
+      } else if (i == "2") {
+        i = "Series SE";
+      }
+      return '<a href="javascript:void(0);" class="dot">' + i + "</a>";
+    },
+  });
 });
